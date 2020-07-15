7ba2bf394f9acdd9994161d7c5efa95972136af5
Hamid Goudarzi <goudarzi.hamid@gmail.com>
2020-07-15 15:17:36 -0400

    Updates

---
+++ precache-manifest.7e019ede364688340faa5f4d52fac34d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a4ee51794bef9918026bd3c3cbbf65c",
+    "url": "/person-manager/index.html"
+  },
+  {
+    "revision": "21599c399a00dbe4e20c",
+    "url": "/person-manager/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "0d67682fcd63f0d3f7e5",
+    "url": "/person-manager/static/css/main.2efb2ef2.chunk.css"
+  },
+  {
+    "revision": "21599c399a00dbe4e20c",
+    "url": "/person-manager/static/js/2.4faac7e1.chunk.js"
+  },
+  {
+    "revision": "11da4d0289ce192632a3fc6590a86fde",
+    "url": "/person-manager/static/js/2.4faac7e1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0d67682fcd63f0d3f7e5",
+    "url": "/person-manager/static/js/main.24cc93b2.chunk.js"
+  },
+  {
+    "revision": "f2e1b4d5b77c30c4227c",
+    "url": "/person-manager/static/js/runtime-main.79b0c01e.js"
+  }
+]);
